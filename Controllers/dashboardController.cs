5024f33740afbc78df30e42ae1f8e3005d0542ca
AmmadRaza93 <90179981+AmmadRaza93@users.noreply.github.com>
2022-05-10 15:42:47 +0500

    .

---
+++ Controllers/dashboardController.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections.Generic;
+using StudioAdmin._Models;
+using StudioAdmin.BLL._Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudioAdmin.Controllers
+{
+    [Route("api/[controller]")]
+
+    public class dashboardController : ControllerBase
+    {
+        dashboardService _service;
+        public dashboardController()
+        {
+            _service = new dashboardService();
+        }
+
+        [HttpGet("get/{LocationID}/{Date}")]
+        public RspDashboard GetDashboardSummary(int LocationID, string Date)
+        {
+            return _service.GetDashboard(LocationID, System.DateTime.Now.Date.ToString());
+        }
+
+        [HttpGet("range/get/{locationID}/{FDate}/{TDate}")]
+        public RspDashboard GetDashboardSummary(int LocationID, string FDate, string TDate)
+        {
+            return _service.GetDashboardRange(LocationID,FDate, TDate);
+        }
+    }
+}
---
+++ Controllers/locationController.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections.Generic;
+using MadinaAdmin._Models;
+using MadinaAdmin.BLL._Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadinaAdmin.Controllers
+{
+    [Route("api/[controller]")]
+  
+    public class locationController : ControllerBase
+    {
+        locationService _service;
+        public locationController()
+        {
+            _service = new locationService();
+        }
+
+        [HttpGet("all/{brandid}")]
+        public List<LocationBLL> GetAll(int brandid)
+        {
+            return _service.GetAll(brandid);
+        }
+
+
+        [HttpGet("{id}/brand/{brandid}")]
+        public LocationBLL Get(int id, int brandid)
+        {
+            return _service.Get(id, brandid);
+        }
+
+        [HttpPost]
+        [Route("insert")]
+        public int Post([FromBody]LocationBLL obj)
+        {
+            return _service.Insert(obj);
+        }
+
+        [HttpPost]
+        [Route("update")]
+        public int PostUpdate([FromBody]LocationBLL obj)
+        {
+            return _service.Update(obj);
+        }
+
+
+        [HttpPost]
+        [Route("delete")]
+        public int PostDelete([FromBody]LocationBLL obj)
+        {
+            return _service.Delete(obj);
+        }
+    }
+}
